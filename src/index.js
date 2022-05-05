34650e92f2c429d2e5a7ade5c287a2bc10ab03d5
Deepak Rawat <dev.deepak.rawat@gmail.com>
2022-05-05 17:57:20 +0400

    Add relative import functionality

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './lib/store';
-import App from './components/App';
+import App from 'components/App';
 import reportWebVitals from './reportWebVitals';
-import './styles/index.css';
+import 'styles/index.css';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
