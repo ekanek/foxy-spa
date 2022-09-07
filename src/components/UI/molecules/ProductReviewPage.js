6dfd2611106a78f3e40f839e4b3fe99419a61b7b
Ashish <sharmaashishlfps@gmail.com>
2022-09-07 13:40:37 +0530

    review page back button fixed

--- src/components/UI/molecules/ProductReviewPage.js
+++ src/components/UI/molecules/ProductReviewPage.js
@@ -3,6 +3,7 @@ import Header from 'components/UI/organisms/Header';
 import RatingsOverviewHeader from 'components/UI/atoms/product-review/RatingsOverviewHeader';
 import CustomerReviewList from 'components/UI/molecules/CustomerReviewList';
 import { useGetProductDetailsQuery } from 'apis/productApis';
+import { useNavigate } from 'react-router-dom';
 const ProductReviewPage = () => {
   const { data } = useGetProductDetailsQuery(
     'api/v2/products/lakme-enrich-matte-lipstick?variant=7848',
@@ -13,9 +14,13 @@ const ProductReviewPage = () => {
     ratings_count: ratingsCount = 0,
     reviews_count: reviewsCount = 0,
   } = data;
+  const navigate = useNavigate();
+  function onPressContainer() {
+    navigate('/');
+  }
   return (
     <div>
-      <Header title="All Reviews" />
+      <Header title="All Reviews" onPress={onPressContainer} />
       <RatingsOverviewHeader
         rating={rating}
         reviewsCount={reviewsCount}
