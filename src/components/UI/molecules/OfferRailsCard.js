ad49a9718ee5ac72afd3a0f8d7a6b8f9eb3a8c1e
Ashish <sharmaashishlfps@gmail.com>
2022-09-08 14:23:20 +0530

    offers card ui updated

--- src/components/UI/molecules/OfferRailsCard.js
+++ src/components/UI/molecules/OfferRailsCard.js
@@ -1,28 +1,28 @@
-import React, { useState } from 'react';
+import React from 'react';
 import styles from 'styles/GridRails.module.scss';
 const OfferRailsCard = (props) => {
-  const [isActive, setIsActive] = useState(false);
-  function handleClick() {
-    setIsActive(!isActive);
-  }
-  let activePromptStyle = isActive ? 'prompt-active' : 'prompt-not-active';
   return (
-    <div className={styles[activePromptStyle]} onClick={handleClick}>
-      <div className={styles['behind-prompt']}>
-        <div className={styles['offer-image']}>
-          <div className={styles['prompt-top']}>
-            <div className={styles['prompt-header']}>
-              <div className={styles['prompt-header-text']}>{props.discount}% off</div>
-              <img height={30} width={30} src={props.url} />
-            </div>
-            <div>
-              <div className={styles['prompt-text']}>{props.text}</div>
-            </div>
+    <div className={styles['offer-card-container']}>
+      <div className={styles['behind-prompt']}></div>
+      <div className={styles['main-card-container']}>
+        <div className={styles['circle-outside-left']}>
+          <div className={styles['circle-inside']}></div>
+        </div>
+        <div className={styles['circle-outside-right']}>
+          <div className={styles['circle-inside']}></div>
+        </div>
+        <div className={styles['prompt-top']}>
+          <div className={styles['prompt-header']}>
+            <div className={styles['prompt-header-text']}>{props.discount}% off</div>
+            <img height={30} width={30} src={props.url} />
           </div>
-          <div className={styles['prompt-bottom-container']}>
-            <span className={styles['prompt-bottom-coupon-code']}>{props.couponCode}</span>
-            <span className={styles['prompt-bottom-coupon-applied']}>{props.buttonText}</span>
+          <div>
+            <div className={styles['prompt-text']}>{props.text}</div>
           </div>
+        </div>
+        <div className={styles['prompt-bottom-container']}>
+          <span className={styles['prompt-bottom-coupon-code']}>{props.couponCode}</span>
+          <span className={styles['prompt-bottom-coupon-applied']}>{props.buttonText}</span>
         </div>
       </div>
     </div>
