19f47de9fd9ff9d37def97d4db30f68e23c07892
Ashish <sharmaashishlfps@gmail.com>
2022-09-20 18:12:51 +0530

    changes images import

--- src/assets/images.js
+++ src/assets/images.js
@@ -1,44 +1,72 @@
 import { APP_ASSET_URL } from 'lib/constants';
-
+import backButton from './images/productPageBack.png';
+import bagButton from './images/bagProductPage@3x.png';
+import searchButton from './images/searchProductPageNew@3x.png';
+import rightArrow from './images/rightChevron@3x.png';
+import orginial from './images/100_percent_originial.png';
+import freeDelivery from './images/free_delivery.png';
+import free_return from './images/hassle_free_returns.png';
+import payments from './images/secure_payments.png';
+import downloadPlayStore from './images/google-play-store-footer.png';
+import instaIcon from './images/insta_ic.png';
+import fbIcon from './images/fb_ic.png';
+import youtubeIcon from './images/youtube_ic.png';
+import twitterIcon from './images/twitter_ic.png';
+import white_star from './images/rating-pill-star-white.png';
+import blue_down_arrow from './images/blue-down-arrow.png';
+import filled_star from './images/star-filled-yellow.png';
+import outline_star from './images/star-outline.png';
+import blueLike from './images/thumb-up-blue.png';
+import greyLike from './images/thumb-up-grey.png';
+import zap from './images/zap.png';
+import addToBagGreen from './images/add-to-bag-green.png';
+import arrow_Left from './images/arrow-left.png';
+import verified_image from './images/verified-image.png';
+import cross_image from './images/cross-image.png';
+import tick_image from './images/tick-mark.png';
+import match_play_image from './images/foxy-match-play.png';
+import match_star_image from './images/star-image.png';
+import addToBag from './images/add_to_bag.png';
+import unlikeImage from './images/unlikeImage.png';
 const images = {
-  backButton: require('./images/productPageBack.png'),
-  bagButton: require('./images/bagProductPage@3x.png'),
-  searchButton: require('./images/searchProductPageNew@3x.png'),
-  rightArrow: require('./images/rightChevron@3x.png'),
+  backButton: backButton,
+  bagButton: bagButton,
+  searchButton: searchButton,
+  rightArrow: rightArrow,
   foxyPromises: {
-    orginial: require('./images/100_percent_originial.png'),
-    freeDelivery: require('./images/free_delivery.png'),
-    return: require('./images/hassle_free_returns.png'),
-    payments: require('./images/secure_payments.png'),
+    orginial: orginial,
+    freeDelivery: freeDelivery,
+    return: free_return,
+    payments: payments,
   },
-  downloadPlayStore: require('./images/google-play-store-footer.png'),
-  instaIcon: require('./images/insta_ic.png'),
-  fbIcon: require('./images/fb_ic.png'),
-  youtubeIcon: require('./images/youtube_ic.png'),
-  twitterIcon: require('./images/twitter_ic.png'),
-  white_star: require('./images/rating-pill-star-white.png'),
-  blue_down_arrow: require('./images/blue-down-arrow.png'),
+  downloadPlayStore: downloadPlayStore,
+  instaIcon: instaIcon,
+  fbIcon: fbIcon,
+  youtubeIcon: youtubeIcon,
+  twitterIcon: twitterIcon,
+  white_star: white_star,
+  blue_down_arrow: blue_down_arrow,
   rating: {
     product_1_star: `${APP_ASSET_URL}/55/one_star.png`,
     product_2_star: `${APP_ASSET_URL}/68/two_star.png`,
     product_3_star: `${APP_ASSET_URL}/61/three_star.png`,
     product_4_star: `${APP_ASSET_URL}/65/four_star.png`,
     product_5_star: `${APP_ASSET_URL}/57/five_star.png`,
-    filled_star: require('./images/star-filled-yellow.png'),
-    outline_star: require('./images/star-outline.png'),
+    filled_star: filled_star,
+    outline_star: outline_star,
   },
-  blueLike: require('./images/thumb-up-blue.png'),
-  greyLike: require('./images/thumb-up-grey.png'),
-  zap: require('./images/zap.png'),
-  addToBagGreen: require('./images/add-to-bag-green.png'),
-  arrow_Left: require('./images/arrow-left.png'),
-  cross_image: require('./images/cross-image.png'),
-  verified_image: require('./images/verified-image.png'),
-  tick_image: require('./images/tick-mark.png'),
-  match_play_image: require('./images/foxy-match-play.png'),
-  match_star_image: require('./images/star-image.png'),
-  addToBag: require('./images/add_to_bag.png'),
-  unlikeImage: require('./images/unlikeImage.png'),
+  blueLike: blueLike,
+  greyLike: greyLike,
+  zap: zap,
+  addToBagGreen: addToBagGreen,
+  arrow_Left: arrow_Left,
+  cross_image: cross_image,
+  verified_image: verified_image,
+  tick_image: tick_image,
+  match_play_image: match_play_image,
+  match_star_image: match_star_image,
+  addToBag: addToBag,
+  unlikeImage: unlikeImage,
 };
 
 export default images;
